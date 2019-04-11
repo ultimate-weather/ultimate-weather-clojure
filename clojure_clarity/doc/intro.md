0d388eb592e41d84c006f7a00a988e333875a676
Michael Durrant <m2@snap2web.com>
2019-04-11 16:07:44 -0400

    clojure

---
+++ clojure_clarity/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure_clarity
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure_clarity/test/clojure_clarity/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-clarity.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-clarity.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
